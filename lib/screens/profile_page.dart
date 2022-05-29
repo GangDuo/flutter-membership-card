d39d5cf537ba7a183c9d32edd76e29015e2b4552
Onoda <onoda-y-xyz@mve.biglobe.ne.jp>
2022-05-29 21:19:23 +0900

    空のプロフィールページを追加。#5

---
+++ lib/screens/profile_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: double.infinity,
+      child: Text("Profile")
+    );
+  }
+}
