96b58a70d2ab5201d605868374beafc239eb0682
Onoda <onoda-y-xyz@mve.biglobe.ne.jp>
2022-06-08 22:42:00 +0900

    ファイル分割。

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,9 @@
  * design:dribble.com
  */
 import 'package:flutter/material.dart';
-import 'screens/cards_page.dart';
-import 'screens/homapage.dart';
-import 'screens/barcode_page.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'components/wallet_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,63 +27,6 @@ class WalletAppClone extends StatelessWidget {
       ),
       home: WalletApp(),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class WalletApp extends StatefulWidget {
-  @override
-  _WalletAppState createState() => _WalletAppState();
-}
-
-class _WalletAppState extends State<WalletApp> {
-  var screens = [
-    HomePage(),
-    CardPage(),
-  ];
-
-  int selectedTab = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(38, 81, 158, 1),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.credit_card), label: "Card"),
-        ],
-        onTap: (index) {
-          setState(() {
-            selectedTab = index;
-          });
-        },
-        showUnselectedLabels: true,
-        iconSize: 30,
-      ),
-      floatingActionButton: SizedBox(
-        height: 72,
-        width: 72,
-        child: FloatingActionButton(
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                return BarcodePage();
-              }));
-            },
-            elevation: 0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(CupertinoIcons.barcode),
-                Text('使う'),
-              ],
-            ),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      body: screens[selectedTab],
     );
   }
 }
